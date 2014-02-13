f965f43cfa184999358cfa52ecc15d86e6714ac4
Mason Fabel <mason.fabel@gmail.com>
2014-02-13 14:29:03 -0800

    Created basic program layout.
    Created the basic layout for the calculator program. This includes
    the global stack, and basic error handling functions common to
    every application.
    
    * main.cpp - Added usage() and error() functions. Added argc check.
      Added global stack.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,37 @@
 #include <cstdlib>
 #include <iostream>
 
+#include "stack.h"
+
 using namespace std;
 
+void usage(char* name);
+void error(char* msg);
+
+Stack operands;
+
+/* Run an interactive RPN calculator */
 int main(int argc, char** argv) {
+	/* Check for the proper amount of arguments */
+	if(argc != 1) {
+		usage(argv[0]);
+		exit(0);
+	}
+
 	exit(0);
+}
+
+
+/* Print usage information */
+void usage(char* name) {
+	cout << "Usage: " << name << endl;
+
+	return;
+}
+
+/* Print an error message and exit */
+void error(char* msg) {
+	cerr << "Error: " << msg << " - exiting." << endl;
+
+	exit(1);
 }
